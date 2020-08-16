49595ab0eb04cc9bd755cd1e6c7c813775d5167a
kemokemo <t2wonderland@gmail.com>
2020-08-16 19:41:05 +0900

    chore: add samples for multi-platform

---
+++ samples/sample.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+colout info "Let's begin my work."
+
+colout success "Successfully done."
+
+colout warn "Please check warnings of logs."
+
+colout error "Oops, an error has occurred."
